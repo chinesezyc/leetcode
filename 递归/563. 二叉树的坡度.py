0131d1275c2b37ef215601f51760dda151c5ff57
zyc <chinesexzyc@gmail.com>
2021-04-01 23:21:10 +0800

    [feat] 563. 二叉树的坡度 【更优的解法】

--- 递归/563. 二叉树的坡度.py
+++ 递归/563. 二叉树的坡度.py
@@ -18,3 +18,20 @@ class Solution:
         return abs(compute_sum(root.left) - compute_sum(root.right)) \
                + self.findTilt(root.left) \
                + self.findTilt(root.right)
+
+class Solution2(object):
+    def __init__(self):
+        self.tilt = 0
+
+    def findTilt(self, root: TreeNode) -> int:
+        self.traverse(root)
+        return self.tilt
+
+    def traverse(self, root: TreeNode) -> int:
+        if root is None:
+            return 0
+
+        left = self.traverse(root.left)
+        right = self.traverse(root.right)
+        self.tilt += abs(left-right)
+        return left+right+root.val
