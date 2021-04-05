4688befed657f3f4b5257bb7cbc1c5230a7bd6a8
zyc <chinesexzyc@gmail.com>
2021-04-05 22:48:49 +0800

    [feat] 1137. 第 N 个泰波那契数【备忘录递归解法二】

--- 递归/1137. 第 N 个泰波那契数.py
+++ 递归/1137. 第 N 个泰波那契数.py
@@ -9,7 +9,6 @@ class Solution:
 
         return fab(n)
 
-
     def tribonacci2(self, n: int) -> int:
         if n == 0:
             return 0
@@ -19,6 +18,23 @@ class Solution:
         for _ in range(3, n + 1):
             i, j, k = j, k, i + j + k
         return k
+
+    def tribonacci(self, n: int) -> int:
+        helper = dict()
+
+        def fab(n: int) -> int:
+            if helper.__contains__(n):
+                return helper[n]
+            if n == 0:
+                helper[0] = 0
+                return 0
+            if n <= 2:
+                helper[n] = 1
+                return 1
+            helper[n] = fab(n - 1) + fab(n - 2) + fab(n - 3)
+            return helper[n]
+
+        return fab(n)
 
 
 if __name__ == "__main__":
