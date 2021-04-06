520ad2cb084315b76a7f0c8a26b8abe0e94eebe4
zyc <chinesexzyc@gmail.com>
2021-04-06 23:13:59 +0800

    [feat] 剑指 Offer 10- II. 青蛙跳台阶问题【DP解法3】

--- 递归/剑指 Offer 10- II. 青蛙跳台阶问题.py
+++ 递归/剑指 Offer 10- II. 青蛙跳台阶问题.py
@@ -9,22 +9,26 @@ class Solution:
 
         return fab(n)
 
-    def numWays(self, n: int) -> int:
+    def numWays2(self, n: int) -> int:
         helper = dict()
 
         def fab(n: int) -> int:
             if helper.__contains__(n):
                 return helper[n]
-            if n == 0:
-                helper[n] = 1
-                return helper[n]
-            if n <= 2:
+            if n <= 1:
                 helper[n] = n
                 return helper[n]
             helper[n] = fab(n - 1) + fab(n - 2)
             return helper[n]
 
-        return fab(n)%1000000007
+        return fab(n) % 1000000007
+
+    def numWays(self, n: int) -> int:
+        i, j = 1, 1
+        for _ in range(n - 1):
+            i, j = j, i + j
+        return j % 1000000007
+
 
 if __name__ == "__main__":
     solution = Solution()
