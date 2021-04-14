3d47ae03eeefbefaa2fbf483050529d6d4c38a81
zyc <chinesexzyc@gmail.com>
2021-04-14 22:44:33 +0800

    [feat] 538. 把二叉搜索树转换为累加树【中序遍历的拉跨解法1】

---
+++ 递归/中等/538. 把二叉搜索树转换为累加树.py
@@ -0,0 +1,44 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def convertBST(self, root: TreeNode) -> TreeNode:
+        sorted_list = []
+
+        def inoder(root: TreeNode):
+            if root is None:
+                return
+            inoder(root.left)
+            sorted_list.append(root.val)
+            inoder(root.right)
+
+        inoder(root)
+        tmp = 0
+        sum_dict = {}
+        for i in sorted_list[::-1]:
+            tmp += i
+            sum_dict[i] = tmp
+
+        def helper(root: TreeNode):
+            if root is None:
+                return
+            helper(root.left)
+            root.val = sum_dict[root.val]
+            helper(root.right)
+
+        helper(root)
+        return root
+if __name__ == '__main__':
+    sorted_list=[1,2,3,4,5]
+    tmp=0
+    sum_dict={}
+    for i in sorted_list[::-1]:
+        tmp+=i
+        sum_dict[i]=tmp
+
+    print(sum_dict)
