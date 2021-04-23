0ef7afa4f778370f4c44bfee728ccb731772c735
zyc <chinesexzyc@gmail.com>
2021-04-23 23:42:20 +0800

    [feat] 46. 全排列【回溯解法二】

--- 回溯/46. 全排列.py
+++ 回溯/46. 全排列.py
@@ -15,10 +15,26 @@ class Solution:
             self.backtrace(nums, trace)
             trace.pop(-1)
 
-    def permute(self, nums: List[int]) -> List[List[int]]:
+    def permute1(self, nums: List[int]) -> List[List[int]]:
 
         self.backtrace(nums, [])
         return self.res
+
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        res = []
+
+        def backtrace(nums: List[int], trace: List[int]):
+            if len(nums) == len(trace):
+                res.append(trace.copy())
+            for each in nums:
+                if each in trace:
+                    continue
+                trace.append(each)
+                backtrace(nums, trace)
+                trace.pop(-1)
+
+        backtrace(nums, [])
+        return res
 
 
 if __name__ == "__main__":
