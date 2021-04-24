0dc550826f37b5e53c81fb86def9163298433e8a
zyc <chinesexzyc@gmail.com>
2021-04-24 23:24:47 +0800

    [feat] 46. 全排列2【回溯解法1】剪枝

--- 回溯/46. 全排列2.py
+++ 回溯/46. 全排列2.py
@@ -5,11 +5,14 @@ class Solution:
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
         res = []
 
-        def backtrace(trace: List[int], tmp_nums: List[int]):
+        def backtrace(trace: List[int], tmp_nums: List[int], pre_choice: int = None):
             if len(tmp_nums) == 0:
                 if trace not in res:
                     res.append(trace.copy())
             for idx in range(len(tmp_nums)):
+                if pre_choice == tmp_nums[idx]:
+                    continue
+                pre_choice = tmp_nums[idx]
                 each = tmp_nums.pop(idx)
                 trace.append(each)
                 backtrace(trace, tmp_nums)
@@ -22,5 +25,5 @@ class Solution:
 
 if __name__ == "__main__":
     solution = Solution()
-    ret = solution.permuteUnique(nums=[1, 1, 2])
+    ret = solution.permuteUnique(nums=[3, 3, 0, 3])
     print(ret)
