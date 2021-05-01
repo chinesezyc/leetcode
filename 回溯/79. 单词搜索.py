ae16cffe4176d8f25ac38ddc03d90509c8254ebe
zyc <chinesexzyc@gmail.com>
2021-05-01 22:25:57 +0800

    [feat] 79. 单词搜索

--- 回溯/79. 单词搜索.py
+++ 回溯/79. 单词搜索.py
@@ -1,32 +1,13 @@
+import sys
 from typing import List
 
 
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         res = []
-        left, right, length = 0, 0, 0
 
         def backtrace(trace: str):
-            nonlocal left, right, length
-            if length == 2 * n:
-                res.append(trace)
-                return
-
-            if left < n:
-                trace += '('
-                left += 1
-                length += 1
-                backtrace(trace)
-                trace = trace[:-1]
-                left -= 1
-                length -= 1
-            if right < left:
-                trace += ')'
-                right += 1
-                length += 1
-                backtrace(trace)
-                right -= 1
-                length -= 1
+            pass
 
         backtrace('')
         return res
