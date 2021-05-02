ebc0b82ab4568087ffa3fbbcc6e0f206577dfcb2
zyc <chinesexzyc@gmail.com>
2021-05-02 23:12:14 +0800

    [feat] 89. 格雷编码

--- 回溯/89. 格雷编码.py
+++ 回溯/89. 格雷编码.py
@@ -1,2 +1,18 @@
+from typing import List
+
+
 class Solution:
     def grayCode(self, n: int) -> List[int]:
+        res = []
+
+        def backtrace(trace: str):
+            pass
+
+        backtrace('')
+        return res
+
+
+if __name__ == "__main__":
+    solution = Solution()
+    ret = solution.grayCode(n=4)
+    print(ret)
