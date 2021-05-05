75ad093b048d804136dbdf05f8416ad04ec73b34
zyc <chinesexzyc@gmail.com>
2021-05-06 00:09:14 +0800

    [feat] 79. 单词搜索[回溯法]剪枝3，去掉flag数组

--- 回溯/79. 单词搜索.py
+++ 回溯/79. 单词搜索.py
@@ -6,36 +6,37 @@ import numpy as np
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         width, height = len(board), len(board[0])
-        flags = np.zeros(shape=(width, height), dtype=np.int)
         board_ = np.zeros(shape=(width, height), dtype=np.int)
         for i in range(width):
             for j in range(height):
                 board_[i, j] = ord(board[i][j])
 
         def backtrace(trace: str, w: int, h: int, idx: int) -> bool:
-            nonlocal flags, board_, width, height
+            nonlocal board_, width, height
             if trace == word:
                 return True
             for m, n in [[-1, 0], [1, 0], [0, -1], [0, 1], ]:
                 if 0 <= w + m < width \
                         and 0 <= h + n < height \
                         and board_[w + m, h + n] == ord(word[idx]) \
-                        and flags[w + m, h + n] == 0:
-                    flags[w + m, h + n] = 1
+                        and board_[w + m, h + n] != -1:
+                    tmp = board_[w + m, h + n]
+                    board_[w + m, h + n] = -1
                     trace += word[idx]
                     if backtrace(trace, w + m, h + n, idx + 1):
                         return True
                     trace = trace[:-1]
-                    flags[w + m, h + n] = 0
+                    board_[w + m, h + n] = tmp
             return False
 
         for i in range(width):
             for j in range(height):
                 if board_[i, j] == ord(word[0]):
-                    flags[i, j] = 1
+                    tmp = board_[i, j]
+                    board_[i, j] = -1
                     if backtrace(word[0], i, j, 1):
                         return True
-                    flags[i, j] = 0
+                    board_[i, j] = tmp
         return False
 
 
