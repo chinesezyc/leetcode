390e3ed7aed3f10c3f839b1cc26eccee2fe7bd54
zyc <chinesexzyc@gmail.com>
2021-05-05 23:23:42 +0800

    [feat] 79. 单词搜索[debug]

--- 回溯/79. 单词搜索.py
+++ 回溯/79. 单词搜索.py
@@ -1,16 +1,37 @@
 import sys
 from typing import List
+import numpy as np
 
 
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        res = []
+        w, h = len(board), len(board[0])
+        flags = np.zeros(shape=(w, h), dtype=np.int)
+        board_ = np.zeros(shape=(w, h), dtype=np.int)
+        for i in range(w):
+            for j in range(h):
+                board_[i, j] = ord(board[i][j])
 
-        def backtrace(trace: str):
-            pass
+        def backtrace(trace: str, w: int, h: int, idx: int) -> bool:
+            nonlocal flags, board_
+            if trace == word:
+                return True
+            if board_[w, h] == ord(trace[-1]):
+                for m in [-1, 1]:
+                    for n in [-1, 1]:
+                        if board_[w + m, h + n] == ord(word[idx]):
+                            trace += word[m]
+                            backtrace(trace, w + m, h + n, idx + 1)
+                            trace = trace[:-1]
 
-        backtrace('')
-        return res
+        for i in range(w):
+            for j in range(h):
+                if board_[i, j] == ord(word[0]):
+                    flags[i, j] = 1
+                    if backtrace(word[0], i, j, 1):
+                        return True
+
+        return False
 
 
 if __name__ == "__main__":
