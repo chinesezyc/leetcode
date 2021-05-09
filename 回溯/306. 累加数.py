acf20024b1d7151773aeccfa47793f8e7e080558
zyc <chinesexzyc@gmail.com>
2021-05-09 23:33:54 +0800

    [feat] 306. 累加数

--- 回溯/306. 累加数.py
+++ 回溯/306. 累加数.py
@@ -5,19 +5,19 @@ class Solution:
     def isAdditiveNumber(self, num: str) -> bool:
 
         def backtrace(start_idx: int, val1: int, val2: int) -> bool:
-            if start_idx == len(num):
+            if start_idx-1 == len(num):
                 return True
 
-            for i in range(start_idx, len(num)):
-                if num[i] == '0':
+            for i in range(start_idx+1, len(num)+1):
+                if num[i-1] == '0':
                     break
-                if val1 and val2 and val1 + val2 != int(num[start_idx:start_idx + i + 1]):
+                if val1 and val2 and val1 + val2 != int(num[start_idx:start_idx + i]):
                     break
 
                 tmp = val1
                 val1 = val2
-                val2 = int(num[start_idx:start_idx + i + 1])
-                if backtrace(i + 1, val1, val2):
+                val2 = int(num[start_idx:i])
+                if backtrace(i, val1, val2):
                     return True
                 val2 = val1
                 val1 = tmp
@@ -28,5 +28,5 @@ class Solution:
 
 if __name__ == "__main__":
     solution = Solution()
-    ret = solution.isAdditiveNumber(num="113")
+    ret = solution.isAdditiveNumber(num="112")
     print(ret)
