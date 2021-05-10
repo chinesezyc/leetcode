673de582c924f09c952108c316437cbb2865dfee
zyc <chinesexzyc@gmail.com>
2021-05-10 23:33:12 +0800

    [feat] 357. 计算各个位数不同的数字个数

--- 回溯/357. 计算各个位数不同的数字个数/source.py
+++ 回溯/357. 计算各个位数不同的数字个数/source.py
@@ -4,24 +4,23 @@ from typing import List
 class Solution:
     def countNumbersWithUniqueDigits(self, n: int) -> int:
 
-        res = []
+        def backtrace(idx: int, use_bool: List[bool]) -> int:
+            count = 0
+            if n != idx:
+                for i in range(0, 10):
+                    if idx == 1 and i == 0 and n > 1:
+                        continue
+                    if use_bool[i]:
+                        continue
+                    use_bool[i] = True
+                    count += backtrace(idx + 1, use_bool) + 1
+                    use_bool[i] = False
+            return count
 
-        def backtrace(trace:str):
-            if n == len(trace):
-                res.append(int(trace))
-
-            for each in nums:
-                if each in trace:
-                    continue
-                trace.append(each)
-                backtrace(trace)
-                trace.pop(-1)
-
-        backtrace([])
-        return res
+        return backtrace(0, [False] * 10)
 
 
 if __name__ == "__main__":
     solution = Solution()
-    ret = solution.permute(nums=[1, 2, 3])
+    ret = solution.countNumbersWithUniqueDigits(n=2)
     print(ret)
