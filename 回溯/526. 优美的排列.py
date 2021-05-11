73d8e6b3ab30e78537a94d1216a23fb51e544335
zyc <chinesexzyc@gmail.com>
2021-05-11 23:49:12 +0800

    [feat] 526. 优美的排列[回溯]

--- 回溯/526. 优美的排列.py
+++ 回溯/526. 优美的排列.py
@@ -3,29 +3,29 @@ from typing import List
 
 class Solution:
     def countArrangement(self, n: int) -> int:
-        if n == 0:
-            return 1
-        count = 0
+        res = []
 
-        def backtrace(idx: int, use_bool: List[bool]) -> int:
-            nonlocal count
-            if n != idx:
-                for i in range(0, 3):
-                    if idx == 1 and i == 0 and n > 1:
-                        continue
-                    if use_bool[i]:
-                        continue
+        def backtrace(trace: List[int], idx: int, use_bool: List[bool]):
+            if n == idx:
+                res.append(trace.copy())
+                return
+            for i in range(1, n + 1):
+                if idx == 1 and i == 0 and n > 1:
+                    continue
+                if use_bool[i]:
+                    continue
+                if i % (len(trace) + 1) == 0 or (len(trace) + 1) % i == 0:
                     use_bool[i] = True
-                    backtrace(idx + 1, use_bool)
+                    trace.append(i)
+                    backtrace(trace, idx + 1, use_bool)
                     use_bool[i] = False
-            else:
-                count += 1
-            return count
+                    trace.pop(-1)
 
-        return backtrace(0, [False] * 10)
+        backtrace([], 0, [False] * (n+1))
+        return len(res)
 
 
 if __name__ == "__main__":
     solution = Solution()
-    ret = solution.countArrangement(n=2)
+    ret = solution.countArrangement(n=10)
     print(ret)
