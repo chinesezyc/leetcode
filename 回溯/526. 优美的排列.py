f1386d4e5a9429b16d1801a2d0716b8387b47ae9
zyc <chinesexzyc@gmail.com>
2021-05-12 23:11:30 +0800

    [feat] 526. 优美的排列[回溯]优化2

--- 回溯/526. 优美的排列.py
+++ 回溯/526. 优美的排列.py
@@ -5,24 +5,20 @@ class Solution:
     def countArrangement(self, n: int) -> int:
         res = 0
 
-        def backtrace(trace: int, idx: int, use_bool: List[bool]):
+        def backtrace(trace: int, use_bool: List[bool]):
             nonlocal res
-            if n == idx:
+            if trace > n:
                 res += 1
-                return
+                # return
             for i in range(1, n + 1):
-                if idx == 1 and i == 0 and n > 1:
-                    continue
                 if use_bool[i]:
                     continue
                 if i % trace == 0 or trace % i == 0:
                     use_bool[i] = True
-                    trace += 1
-                    backtrace(trace, idx + 1, use_bool)
+                    backtrace(trace + 1, use_bool)
                     use_bool[i] = False
-                    trace -= 1
 
-        backtrace(1, 0, [False] * (n + 1))
+        backtrace(1, [False] * (n + 1))
         return res
 
 
