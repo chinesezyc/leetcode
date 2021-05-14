9dbf9b63227f61c3761d343d5d8dc34e38e99258
zyc <chinesexzyc@gmail.com>
2021-05-14 23:09:56 +0800

    [feat] 1849. 将字符串拆分为递减的连续值[回溯]

---
+++ 回溯/1849. 将字符串拆分为递减的连续值.py
@@ -0,0 +1,24 @@
+class Solution:
+    def splitString(self, s: str) -> bool:
+        length = len(s)
+
+        def backtrace(pre_num: int, start: int, nums: int):
+            nonlocal length
+            if start >= length and nums > 1:
+                return True
+
+            for i in range(start, length):
+                val_int = int(s[start:i + 1])
+                if pre_num - val_int != 1 and nums:
+                    continue
+                if backtrace(val_int, i + 1, nums + 1):
+                    return True
+            return False
+
+        return backtrace(-2, 0, 0)
+
+
+if __name__ == "__main__":
+    solution = Solution()
+    ret = solution.splitString(s="141312000011")
+    print(ret)
