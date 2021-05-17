c4737c15bb041ef387e32b6964ca5d73f232729d
zhangyichen <chinesexzyc@gmail.com>
2021-05-17 23:13:46 +0800

    [feat] 1079. 活字印刷[回溯]按剩余字母个数回溯，天然去重

--- 回溯/1079. 活字印刷.py
+++ 回溯/1079. 活字印刷.py
@@ -3,26 +3,23 @@ from typing import List
 
 class Solution:
     def numTilePossibilities(self, tiles: str) -> int:
-        length = len(tiles)
-        tiles_list = sorted(tiles)
+        tiles_list = [0] * 26
+        for c in tiles:
+            tiles_list[ord(c) - 65] += 1
         res = 0
 
-        def backtrace(used: List[bool], num: int):
-            nonlocal length, res
-            if num > length:
-                return
+        def backtrace():
+            nonlocal tiles_list, res
 
-            for i in range(length):
-                if used[i]:
+            for i in range(26):
+                if tiles_list[i] == 0:
                     continue
-                if i - 1 >= 0 and tiles_list[i] == tiles_list[i - 1] and used[i - 1] is False:
-                    continue
-                used[i] = True
+                tiles_list[i] -= 1
                 res += 1
-                backtrace(used, num + 1)
-                used[i] = False
+                backtrace()
+                tiles_list[i] += 1
 
-        backtrace([False] * length, 1)
+        backtrace()
         return res
 
 
