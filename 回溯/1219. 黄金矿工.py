adc3b8a75e2d7dec6f294d45db9ef663825448fe
zhangyichen <chinesexzyc@gmail.com>
2021-05-18 23:57:38 +0800

    [feat] 1219. 黄金矿工[回溯]AC,优化

--- 回溯/1219. 黄金矿工.py
+++ 回溯/1219. 黄金矿工.py
@@ -6,45 +6,40 @@ class Solution:
         width, height = len(grid[0]), len(grid)
         used = [[False] * width for _ in range(height)]
         path = [[0, -1], [0, 1], [-1, 0], [1, 0]]
-        res = []
+        res = 0
 
-        def check_good_path(h: int, w: int):
+        def generate_next_steps(h: int, w: int):
             nonlocal path, width, height
-            tmp = []
+            next_step = []
             num = 0
             for i, j in path:
                 if -1 < h + i < height \
                         and -1 < w + j < width \
                         and used[h + i][w + j] is False \
                         and grid[h + i][w + j] != 0:
-                    tmp.append([h + i, w + j])
+                    next_step.append([h + i, w + j])
                     num += 1
-            return tmp, num
+            return next_step, num
 
-        def backtrace(h: int, w: int, val: int, trace: List[int]):
+        def backtrace(h: int, w: int, val: int):
             nonlocal res, used
-            tmp, num = check_good_path(h, w)
+            next_step, num = generate_next_steps(h, w)
             if num == 0:
-                res.append(val)
-            for i, j in tmp:
-                if used[i][j] or grid[i][j] == 0:
-                    continue
-                val += grid[i][j]
-                used[i][j] = True
-                trace.append(grid[i][j])
-                backtrace(i, j, val, trace)
-                val -= grid[i][j]
-                used[i][j] = False
-                trace.pop(-1)
+                res = max(res, val)
+            else:
+                for i, j in next_step:
+                    used[i][j] = True
+                    backtrace(i, j, val + grid[i][j])
+                    used[i][j] = False
 
         for h in range(height):
             for w in range(width):
                 if grid[h][w] != 0:
                     used[h][w] = True
-                    backtrace(h, w, grid[h][w], [grid[h][w]])
+                    backtrace(h, w, grid[h][w])
                     used[h][w] = False
 
-        return max(res)
+        return res
 
 
 if __name__ == "__main__":
