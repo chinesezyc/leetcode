e31c8d58b5e6073066aa8cec6f55bd0e7261f62b
zhangyichen <chinesexzyc@gmail.com>
2021-05-18 23:42:27 +0800

    [feat] 1219. 黄金矿工[回溯]

--- 回溯/1219. 黄金矿工.py
+++ 回溯/1219. 黄金矿工.py
@@ -6,14 +6,17 @@ class Solution:
         width, height = len(grid[0]), len(grid)
         used = [[False] * width for _ in range(height)]
         path = [[0, -1], [0, 1], [-1, 0], [1, 0]]
-        res = []
+        res = -1
 
         def check_good_path(w: int, h: int):
             nonlocal path, width, height
             tmp = []
             num = 0
             for i, j in path:
-                if -1 < h + i < height and -1 < w + j < width:
+                if -1 < h + i < height \
+                        and -1 < w + j < width \
+                        and used[h + i][w + j] is False \
+                        and grid[h + i][w + j] != 0:
                     tmp.append([h + i, w + j])
                     num += 1
             return tmp, num
@@ -22,10 +25,8 @@ class Solution:
             nonlocal res, used
             tmp, num = check_good_path(w, h)
             if num == 0:
-                res.append(val)
+                res = max(res, val)
             for i, j in tmp:
-                if used[i][j]:
-                    continue
                 val += grid[i][j]
                 used[i][j] = True
                 backtrace(i, j, val)
@@ -44,5 +45,5 @@ class Solution:
 
 if __name__ == "__main__":
     solution = Solution()
-    ret = solution.getMaximumGold(grid=[[0, 6, 0], [5, 8, 7], [0, 9, 0]])
+    ret = solution.getMaximumGold(grid=[[1,0,7],[2,0,6],[3,4,5],[0,3,0],[9,0,20]])
     print(ret)
