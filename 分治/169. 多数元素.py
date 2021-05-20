f51d35ef4401ff99df032d6384bc28429c425f03
zhangyichen <chinesexzyc@gmail.com>
2021-05-20 22:36:22 +0800

    [feat] 169. 多数元素

---
+++ 分治/169. 多数元素.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        pass
+
+
+if __name__ == "__main__":
+    solution = Solution()
+    ret = solution.majorityElement([2, 2, 1, 1, 1, 2, 2])
+    print(ret)
