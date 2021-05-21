3d1b946b20bf51ae1de90e0274368f8dc221dbce
zyc <chinesexzyc@gmail.com>
2021-05-21 23:50:13 +0800

    [feat] 剑指 Offer 25. 合并两个排序的链表[递归分治？]

--- 分治/剑指 Offer 25. 合并两个排序的链表.py
+++ 分治/剑指 Offer 25. 合并两个排序的链表.py
@@ -10,4 +10,16 @@ class ListNode:
 
 class Solution:
     def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
-        pass
+        if l1 is None and l2 is None:
+            return None
+        if l1 is None:
+            return l2
+        if l2 is None:
+            return l1
+        if l1.val<l2.val:
+            l1.next=self.mergeTwoLists(l1.next,l2)
+            return l1
+        else:
+            l2.next=self.mergeTwoLists(l1,l2.next)
+            return l2
+
