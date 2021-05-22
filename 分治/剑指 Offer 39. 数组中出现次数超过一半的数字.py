3fbc98ffa5a19dad30a4d2aeecab3211ab6e1635
zyc <chinesexzyc@gmail.com>
2021-05-22 23:54:18 +0800

    [feat] 剑指 Offer 39. 数组中出现次数超过一半的数字

---
+++ 分治/剑指 Offer 39. 数组中出现次数超过一半的数字.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        pass
+
+
+if __name__ == "__main__":
+    solution = Solution()
+    ret = solution.majorityElement(nums=[1, 2, 3, 2, 2, 2, 5, 4, 2])
+    print(ret)
