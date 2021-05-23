9e64d48466b389fa36047b08f32c9d76796777c9
zyc <chinesexzyc@gmail.com>
2021-05-23 23:23:44 +0800

    [feat] 剑指 Offer 40. 最小的k个数

---
+++ 分治/剑指 Offer 40. 最小的k个数.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+class Solution:
+    def getLeastNumbers(self, arr: List[int], k: int) -> List[int]:
+        pass
+
+
+if __name__ == "__main__":
+    solution = Solution()
+    ret = solution.getLeastNumbers(arr=[3, 2, 1], k=2)
+    print(ret)
