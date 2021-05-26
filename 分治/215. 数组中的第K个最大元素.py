5572bb1c3d92f3f591a1346af996dcc9f198a726
zyc <chinesexzyc@gmail.com>
2021-05-27 00:10:48 +0800

    [feat] 215. 数组中的第K个最大元素[二分查找]

--- 分治/215. 数组中的第K个最大元素.py
+++ 分治/215. 数组中的第K个最大元素.py
@@ -1,10 +1,29 @@
 from typing import List
+import random
 
 
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        nums.sort(reverse=True)
-        return nums[k - 1]
+        def findTopKth(low, high):
+            pivot = random.randint(low, high)
+            pivot=0
+            nums[low], nums[pivot] = nums[pivot], nums[low]
+            base = nums[low]
+            i = low
+            j = low + 1
+            while j <= high:
+                if nums[j] > base:
+                    nums[i + 1], nums[j] = nums[j], nums[i + 1]
+                    i += 1
+                j += 1
+            nums[low], nums[i] = nums[i], nums[low]
+            if i == k - 1:
+                return nums[i]
+            elif i > k - 1:
+                return findTopKth(low, i - 1)
+            else:
+                return findTopKth(i + 1, high)
+        return findTopKth(0, len(nums) - 1)
 
 
 if __name__ == "__main__":
