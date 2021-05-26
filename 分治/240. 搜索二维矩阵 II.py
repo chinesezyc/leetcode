cbab7510c19ea9c726ac9e104695569a8a31566f
zyc <chinesexzyc@gmail.com>
2021-05-26 23:34:20 +0800

    [feat] 240. 搜索二维矩阵 II[规律解法]

--- 分治/240. 搜索二维矩阵 II.py
+++ 分治/240. 搜索二维矩阵 II.py
@@ -3,7 +3,16 @@ from typing import List
 
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        pass
+        h, w = len(matrix) - 1, 0
+        while h >= 0 and w < len(matrix[0]):
+            if matrix[h][w] == target:
+                return True
+            elif matrix[h][w] > target:
+                h -= 1
+            else:
+                w += 1
+
+        return False
 
 
 if __name__ == "__main__":
