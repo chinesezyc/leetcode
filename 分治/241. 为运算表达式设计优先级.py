2cc935bb15df0cc510ab30c7f16a3256fa786cd9
zyc <chinesexzyc@gmail.com>
2021-05-30 23:45:32 +0800

    [feat] 241. 为运算表达式设计优先级

---
+++ 分治/241. 为运算表达式设计优先级.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+class Solution:
+    def diffWaysToCompute(self, expression: str) -> List[int]:
+        pass
+
+
+if __name__ == "__main__":
+    solution = Solution()
+    ret = solution.diffWaysToCompute("2-1-1")
+    print(ret)
