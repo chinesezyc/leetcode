48d7c4ae712d77db7c57dadcca6cebd134ab6d14
zhangyichen <chinesexzyc@gmail.com>
2021-05-31 23:13:58 +0800

    [feat] 241. 为运算表达式设计优先级[分治]

--- 分治/241. 为运算表达式设计优先级.py
+++ 分治/241. 为运算表达式设计优先级.py
@@ -3,7 +3,22 @@ from typing import List
 
 class Solution:
     def diffWaysToCompute(self, expression: str) -> List[int]:
-        pass
+        if expression.isdigit():
+            return [int(expression)]
+        res = []
+        for i, c in enumerate(expression):
+            if c in ['+', '-', '*']:
+                left = self.diffWaysToCompute(expression[:i])
+                right = self.diffWaysToCompute(expression[i + 1:])
+                for l in left:
+                    for r in right:
+                        if c == '-':
+                            res.append(l - r)
+                        elif c == '+':
+                            res.append(l + r)
+                        else:
+                            res.append(l * r)
+        return res
 
 
 if __name__ == "__main__":
