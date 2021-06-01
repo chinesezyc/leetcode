7d40c10e70fdb99caf15b0e5daab418fd61a7768
zyc <chinesexzyc@gmail.com>
2021-06-02 00:59:40 +0800

    [feat] 395. 至少有 K 个重复字符的最长子串

---
+++ 分治/395. 至少有 K 个重复字符的最长子串.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+class Solution:
+    def longestSubstring(self, s: str, k: int) -> int:
+        pass
+
+
+if __name__ == "__main__":
+    solution = Solution()
+    ret = solution.longestSubstring(s="aaabb", k=3)
+    print(ret)
