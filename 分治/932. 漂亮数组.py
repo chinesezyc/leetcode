d5ab61ad3211aa719dfd918eba1c1d801461bdba
zyc <chinesexzyc@gmail.com>
2021-06-03 23:58:13 +0800

    [feat] 932. 漂亮数组

---
+++ 分治/932. 漂亮数组.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+class Solution:
+    def beautifulArray(self, n: int) -> List[int]:
+        pass
+
+
+
+if __name__ == "__main__":
+    solution = Solution()
+    ret = solution.beautifulArray(n=4)
+    print(ret)
