3d44ee3ba7c7db03eccfc086a7d91556a3d8fc99
zyc <chinesexzyc@gmail.com>
2021-06-04 23:20:28 +0800

    [feat] 973. 最接近原点的 K 个点[直接解法]

--- 分治/973. 最接近原点的 K 个点.py
+++ 分治/973. 最接近原点的 K 个点.py
@@ -4,11 +4,8 @@ from typing import List
 
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-        dist = {}
-        for i, [x, y] in enumerate(points):
-            dist[i] = (x ** 2 + y ** 2)
-        l = sorted(dist.items(), key=lambda x1: x1[1], reverse=False)
-        return [points[i[0]] for i in l[:k]]
+        points.sort(key=lambda x:x[0]**2+x[1]**2)
+        return points[:k]
 
 
 if __name__ == "__main__":
