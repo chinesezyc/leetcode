1ad20cc27f49b2a37633234f75199362e35254a5
zhangyichen <chinesexzyc@gmail.com>
2021-06-07 23:15:13 +0800

    [feat] 面试题 17.14. 最小K个数

--- 分治/面试题 17.14. 最小K个数.py
+++ 分治/面试题 17.14. 最小K个数.py
@@ -4,34 +4,11 @@ import random
 
 class Solution:
     def smallestK(self, arr: List[int], k: int) -> List[int]:
-        if k == 0:
-            return []
-        if k >= len(arr):
-            return arr
-
-        def findTopKth(low, high):
-            pivot = random.randint(low, high)
-            arr[low], arr[pivot] = arr[pivot], arr[low]
-            base = arr[low]
-            i = low
-            j = low + 1
-            while j <= high:
-                if arr[j] < base:
-                    arr[i + 1], arr[j] = arr[j], arr[i + 1]
-                    i += 1
-                j += 1
-            arr[low], arr[i] = arr[i], arr[low]
-            if i == k - 1:
-                return arr[i]
-            elif i > k - 1:
-                return findTopKth(low, i - 1)
-            else:
-                return findTopKth(i + 1, high)
-
-        return arr[:findTopKth(0, len(arr) - 1)]
+        arr.sort()
+        return arr[:k]
 
 
 if __name__ == "__main__":
     solution = Solution()
-    ret = solution.smallestK(arr=[1, 3, 5, ], k=20)
+    ret = solution.smallestK(arr=[11, 3, 5, ], k=1)
     print(ret)
