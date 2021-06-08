7824836538aca5237982794e99b4c743b04a8761
zhangyichen <chinesexzyc@gmail.com>
2021-06-08 23:32:10 +0800

    [feat] 392. 判断子序列

---
+++ 贪心/392. 判断子序列.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        pass
+
+
+if __name__ == "__main__":
+    solution = Solution()
+    ret = solution.isSubsequence(s = "abc", t = "ahbgdc")
+    print(ret)
