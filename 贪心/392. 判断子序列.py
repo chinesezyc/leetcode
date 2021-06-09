b612875861938faafac0bdaf0d250038d2af2864
zyc <chinesexzyc@gmail.com>
2021-06-09 23:13:08 +0800

    [feat] 392. 判断子序列[贪心]

--- 贪心/392. 判断子序列.py
+++ 贪心/392. 判断子序列.py
@@ -3,10 +3,16 @@ from typing import List
 
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
-        pass
+        start, end = 0, len(s)
+        for c in t:
+            if start >= end:
+                return True
+            if c == s[start]:
+                start += 1
+        return start >= end
 
 
 if __name__ == "__main__":
     solution = Solution()
-    ret = solution.isSubsequence(s = "abc", t = "ahbgdc")
+    ret = solution.isSubsequence(s="abc", t="ahbgdc")
     print(ret)
