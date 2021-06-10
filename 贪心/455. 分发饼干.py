2189e40402fbd4312ad8235ce5c7dc1e5a50337a
zyc <chinesexzyc@gmail.com>
2021-06-11 00:00:54 +0800

    [feat] 455. 分发饼干[贪心]

--- 贪心/455. 分发饼干.py
+++ 贪心/455. 分发饼干.py
@@ -4,10 +4,18 @@ from typing import List
 class Solution:
     def findContentChildren(self, g: List[int], s: List[int]) -> int:
         m, n, i, j = len(g), len(s), 0, 0
-
+        g.sort()
+        s.sort()
+        ret = 0
+        while i < m and j < n:
+            if g[i] <= s[j]:
+                ret += 1
+                i += 1
+            j += 1
+        return ret
 
 
 if __name__ == "__main__":
     solution = Solution()
-    ret = solution.findContentChildren(g=[1, 2, 3], s=[1, 1])
+    ret = solution.findContentChildren(g=[1, 2, 3], s=[1, 2,3,4])
     print(ret)
