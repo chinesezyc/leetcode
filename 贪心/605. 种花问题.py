42e2adca14eddc849c8903584acf6a5e025b6fe2
zyc <chinesexzyc@gmail.com>
2021-06-11 23:55:34 +0800

    [feat] 605. 种花问题

---
+++ 贪心/605. 种花问题.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+class Solution:
+    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+        pass
+
+
+if __name__ == "__main__":
+    solution = Solution()
+    ret = solution.canPlaceFlowers(flowerbed=[1, 0, 0, 0, 1], n=1)
+    print(ret)
