579ac0a5e57ed3de07c1ae5196a91c3398d49fe0
zyc <chinesexzyc@gmail.com>
2021-06-12 23:09:54 +0800

    [feat] 605. 种花问题

--- 贪心/605. 种花问题.py
+++ 贪心/605. 种花问题.py
@@ -3,7 +3,17 @@ from typing import List
 
 class Solution:
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
-        low,high=0,len(flowerbed)
+        flowerbed = [0] + flowerbed + [0]
+        k, i, high = 0, 1, len(flowerbed)
+
+        while i <= high or k > 0:
+            if flowerbed[i - 1] + flowerbed[i] + flowerbed[i + 1] == 0:
+                k -= 1
+                flowerbed[i] = 1
+                i += 1
+            else:
+                i += 1
+        return k == 0
 
 
 if __name__ == "__main__":
