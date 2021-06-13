42400200090d3f26bde9df4daa6d704906c4343a
zyc <chinesexzyc@gmail.com>
2021-06-13 23:32:07 +0800

    [feat] 860. 柠檬水找零

---
+++ 贪心/860. 柠檬水找零.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+class Solution:
+    def lemonadeChange(self, bills: List[int]) -> bool:
+        pass
+
+
+if __name__ == "__main__":
+    solution = Solution()
+    ret = solution.lemonadeChange(bills=[5,5,5,10,20])
+    print(ret)
