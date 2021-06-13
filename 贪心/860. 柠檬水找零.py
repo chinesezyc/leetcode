7d36f11ad0b2259d64a6700ed0561132058d9994
zyc <chinesexzyc@gmail.com>
2021-06-13 23:51:32 +0800

    [feat] 860. 柠檬水找零

--- 贪心/860. 柠檬水找零.py
+++ 贪心/860. 柠檬水找零.py
@@ -3,10 +3,29 @@ from typing import List
 
 class Solution:
     def lemonadeChange(self, bills: List[int]) -> bool:
-        pass
+        i, length = 0, len(bills)
+        five, ten = 0, 0
+        for i in range(length):
+            if bills[i] == 5:
+                five += 1
+            elif bills[i] == 10:
+                if five > 0:
+                    five -= 1
+                    ten += 1
+                else:
+                    return False
+            else:
+                if ten > 0 and five > 0:
+                    five -= 1
+                    ten -= 1
+                elif five >= 3:
+                    five -= 3
+                else:
+                    return False
+        return True
 
 
 if __name__ == "__main__":
     solution = Solution()
-    ret = solution.lemonadeChange(bills=[5,5,5,10,20])
+    ret = solution.lemonadeChange(bills=[5, 5, 5, 10, 20])
     print(ret)
