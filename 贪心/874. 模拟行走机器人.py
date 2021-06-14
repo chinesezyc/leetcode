1ae150af01e7b1114e39a2eca1580b42739a6a43
zyc <chinesexzyc@gmail.com>
2021-06-14 23:14:53 +0800

    [feat] 874. 模拟行走机器人

---
+++ 贪心/874. 模拟行走机器人.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+class Solution:
+    def robotSim(self, commands: List[int], obstacles: List[List[int]]) -> int:
+        pass
+
+
+if __name__ == "__main__":
+    solution = Solution()
+    ret = solution.robotSim(commands=[4, -1, 3], obstacles=[])
+    print(ret)
