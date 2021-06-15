649b741813dbb8e0423aa11cb2b4820c1494e343
zyc <chinesexzyc@gmail.com>
2021-06-16 00:33:45 +0800

    [feat] 944. 删列造序

---
+++ 贪心/944. 删列造序.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+class Solution:
+    def minDeletionSize(self, strs: List[str]) -> int:
+        pass
+
+
+if __name__ == "__main__":
+    solution = Solution()
+    ret = solution.minDeletionSize(strs=["cba", "daf", "ghi"])
+    print(ret)
