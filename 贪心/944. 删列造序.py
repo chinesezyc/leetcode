8ebfa7b9e224dbbc2c4fb699426318322c2aadec
zyc <chinesexzyc@gmail.com>
2021-06-16 22:55:30 +0800

    [feat] 944. 删列造序

--- 贪心/944. 删列造序.py
+++ 贪心/944. 删列造序.py
@@ -3,7 +3,16 @@ from typing import List
 
 class Solution:
     def minDeletionSize(self, strs: List[str]) -> int:
-        pass
+        if strs is None:
+            return 0
+        width, height = len(strs[0]), len(strs)
+        ret = 0
+        for w in range(width):
+            for h in range(1, height):
+                if strs[h][w] < strs[h - 1][w]:
+                    ret += 1
+                    break
+        return ret
 
 
 if __name__ == "__main__":
