6e0213db16e7a54b5bf713d952d17570fc7f4b15
zyc <chinesexzyc@gmail.com>
2021-06-17 23:46:43 +0800

    [feat] 1005. K 次取反后最大化的数组和

---
+++ 贪心/1005. K 次取反后最大化的数组和.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+class Solution:
+    def largestSumAfterKNegations(self, nums: List[int], k: int) -> int:
+        pass
+
+
+if __name__ == "__main__":
+    solution = Solution()
+    ret = solution.largestSumAfterKNegations(A = [4,2,3], K = 1)
+    print(ret)
