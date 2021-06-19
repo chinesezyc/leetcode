d6abd0eebd1aa0b49900e2f84a651ddc74f3f0d2
zyc <chinesexzyc@gmail.com>
2021-06-19 23:28:09 +0800

    [feat] 1217. 玩筹码

---
+++ 贪心/1217. 玩筹码.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+class Solution:
+    def minCostToMoveChips(self, position: List[int]) -> int:
+        pass
+
+
+if __name__ == "__main__":
+    solution = Solution()
+    ret = solution.minCostToMoveChips(chips = [1,2,3])
+    print(ret)
