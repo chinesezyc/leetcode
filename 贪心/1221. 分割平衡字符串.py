42e0370ef2d9db916e31e9c21be56768ad4ab610
zyc <chinesexzyc@gmail.com>
2021-06-20 23:09:45 +0800

    [feat] 1221. 分割平衡字符串

---
+++ 贪心/1221. 分割平衡字符串.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+class Solution:
+    def balancedStringSplit(self, s: str) -> int:
+
+
+
+if __name__ == "__main__":
+    solution = Solution()
+    ret = solution.balancedStringSplit(s = "RLRRLLRLRL")
+    print(ret)
