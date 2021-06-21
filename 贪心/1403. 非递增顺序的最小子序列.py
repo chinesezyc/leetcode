b89e3b3acdf5a60a3707d6d900ae19dfb180f022
zyc <chinesexzyc@gmail.com>
2021-06-21 23:50:36 +0800

    [feat] 1403. 非递增顺序的最小子序列

--- 贪心/1403. 非递增顺序的最小子序列.py
+++ 贪心/1403. 非递增顺序的最小子序列.py
@@ -3,10 +3,16 @@ from typing import List
 
 class Solution:
     def minSubsequence(self, nums: List[int]) -> List[int]:
-
+        nums.sort(reverse=True)
+        result, total, half_num = [], 0, sum(nums) / 2
+        for num in nums:
+            total += num
+            result.append(num)
+            if total > half_num:
+                return result
 
 
 if __name__ == "__main__":
     solution = Solution()
-    ret = solution.minSubsequence(nums = [4,3,10,9,8])
+    ret = solution.minSubsequence(nums=[4, 3, 10, 9, 8])
     print(ret)
