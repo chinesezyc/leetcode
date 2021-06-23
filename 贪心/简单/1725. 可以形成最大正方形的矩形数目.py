c966ae8047c5872af467fd7d1f578e7987282e44
zyc <chinesexzyc@gmail.com>
2021-06-23 23:10:54 +0800

    [feat] 1725. 可以形成最大正方形的矩形数目

--- 贪心/简单/1725. 可以形成最大正方形的矩形数目.py
+++ 贪心/简单/1725. 可以形成最大正方形的矩形数目.py
@@ -3,10 +3,16 @@ from typing import List
 
 class Solution:
     def countGoodRectangles(self, rectangles: List[List[int]]) -> int:
-
+        sides = []
+        max_len = -1
+        for rect in rectangles:
+            val = min(rect)
+            sides.append(val)
+            max_len = max(max_len, val)
+        return sides.count(max_len)
 
 
 if __name__ == "__main__":
     solution = Solution()
-    ret = solution.countGoodRectangles(rectangles = [[5,8],[3,9],[5,12],[16,5]])
+    ret = solution.countGoodRectangles(rectangles=[[6, 8], [3, 9], [5, 12], [16, 5]])
     print(ret)
