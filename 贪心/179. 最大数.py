c027817010c86102a4aa72554af0f469ec334286
zyc <chinesexzyc@gmail.com>
2021-07-04 23:03:41 +0800

    179. 最大数

---
+++ 贪心/179. 最大数.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+class Solution:
+    def largestNumber(self, nums: List[int]) -> str:
+
+
+
+if __name__ == "__main__":
+    solution = Solution()
+    ret = solution.largestNumber(nums = [3,30,34,5,9])
+    print(ret)
