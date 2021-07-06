9e47eec2ec4bf027d66b1dfb7e6dba79f849fc59
zyc <chinesexzyc@gmail.com>
2021-07-06 23:27:01 +0800

    316. 去除重复字母

--- 贪心/316. 去除重复字母.py
+++ 贪心/316. 去除重复字母.py
@@ -4,11 +4,18 @@ from typing import List
 
 class Solution:
     def removeDuplicateLetters(self, s: str) -> str:
-
+        remain_counts = collections.Counter(s)
+        stack = []
+        for c in s:
+            if c not in stack:
+                while stack and stack[-1] > c and remain_counts[stack[-1]]:
+                    stack.pop()
+                stack.append(c)
+            remain_counts[c] -= 1
         return ''.join(stack)
 
 
 if __name__ == "__main__":
     solution = Solution()
-    ret = solution.removeDuplicateLetters(s = "cbacdcbc")
+    ret = solution.removeDuplicateLetters(s="cbacdcbc")
     print(ret)
