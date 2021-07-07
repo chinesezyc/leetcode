028aee4cf4e13dd238882fb403c19779c9dae99d
zyc <chinesexzyc@gmail.com>
2021-07-07 23:10:32 +0800

    334. 递增的三元子序列

---
+++ 贪心/334. 递增的三元子序列.py
@@ -0,0 +1,23 @@
+import collections
+from typing import List
+
+
+class Solution:
+    def increasingTriplet(self, nums: List[int]) -> bool:
+        low, mid = float('inf'), float('inf')
+        for c in nums:
+            if c <= low:
+                low = c
+            elif c <= mid:
+                mid = c
+            else:
+                return True
+        return False
+
+
+
+
+if __name__ == "__main__":
+    solution = Solution()
+    ret = solution.increasingTriplet(nums=[2, 1, 5, 0, 4, 6])
+    print(ret)
