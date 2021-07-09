9ae48cdadf5d841e82bf7f6a0d957f696d0e71c4
zyc <chinesexzyc@gmail.com>
2021-07-09 22:03:52 +0800

    406. 根据身高重建队列

--- 贪心/406. 根据身高重建队列.py
+++ 贪心/406. 根据身高重建队列.py
@@ -3,13 +3,7 @@ from typing import List
 
 class Solution:
     def reconstructQueue(self, people: List[List[int]]) -> List[List[int]]:
-        length = len(nums)
-        dp = [1] * length
-        for i in range(1, length):
-            if nums[i] > nums[i - 1]:
-                dp[i] = dp[i - 1] + 1
 
-        return max(dp)
 
 
 if __name__ == "__main__":
