bad375cc9e4f1e1014b84debdc26dd7503be73a8
zyc <chinesexzyc@gmail.com>
2021-07-14 23:15:09 +0800

    376. 摆动序列

--- 动态规划/376. 摆动序列.py
+++ 动态规划/376. 摆动序列.py
@@ -3,14 +3,9 @@ from typing import List
 
 class Solution:
     def wiggleMaxLength(self, nums: List[int]) -> int:
-        people.sort(key=lambda x: [-x[0], x[1]])
-        ret = []
-        for i in people:
-            ret.insert(i[1], i)
-        return ret
 
 
 if __name__ == "__main__":
     solution = Solution()
-    result = solution.wiggleMaxLength(nums = [1,7,4,9,2,5])
+    result = solution.wiggleMaxLength(nums=[1, 7, 4, 9, 2, 5])
     print(result)
