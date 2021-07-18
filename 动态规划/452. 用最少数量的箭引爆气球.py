880fb4358f2f9c6b2cc8d93bb13e515903d7572b
zyc <chinesexzyc@gmail.com>
2021-07-18 22:01:51 +0800

    452. 用最少数量的箭引爆气球

--- 动态规划/452. 用最少数量的箭引爆气球.py
+++ 动态规划/452. 用最少数量的箭引爆气球.py
@@ -3,9 +3,20 @@ from typing import List
 
 class Solution:
     def findMinArrowShots(self, points: List[List[int]]) -> int:
+        points.sort(key=lambda x: x[1])
+        ret = 0
+        while points:
+            right = points[0][1]
+            ret += 1
+            del points[0]
+            for i in range(len(points) - 1, -1, -1):
+                if points[i][0] <= right <= points[i][1]:
+                    del points[i]
+
+        return ret
 
 
 if __name__ == "__main__":
     solution = Solution()
-    result = solution.findMinArrowShots(points = [[10,16],[2,8],[1,6],[7,12]])
+    result = solution.findMinArrowShots(points=[[10, 16], [2, 8], [1, 6], [7, 12]])
     print(result)
