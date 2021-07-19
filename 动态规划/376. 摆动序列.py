bb252b76b9f09a5bce0a33174013d94d834b34c6
zyc <chinesexzyc@gmail.com>
2021-07-19 22:28:15 +0800

    376. 摆动序列

--- 动态规划/376. 摆动序列.py
+++ 动态规划/376. 摆动序列.py
@@ -3,6 +3,13 @@ from typing import List
 
 class Solution:
     def wiggleMaxLength(self, nums: List[int]) -> int:
+        up, down = 1, 1
+        for i in range(1, len(nums)):
+            if nums[i] > nums[i - 1]:
+                up = down + 1
+            if nums[i] < nums[i - 1]:
+                down = up + 1
+        return 0 if 0 == len(nums) else max(up, down)
 
 
 if __name__ == "__main__":
