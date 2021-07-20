c92e687fad63ef03620041a556bcac78d3d59c5f
zyc <chinesexzyc@gmail.com>
2021-07-20 21:39:12 +0800

    452. 用最少数量的箭引爆气球

---
+++ 动态规划/452. 用最少数量的箭引爆气球.py
@@ -0,0 +1,11 @@
+from typing import List
+
+
+class Solution:
+    def wiggleMaxLength(self, nums: List[int]) -> int:
+
+
+if __name__ == "__main__":
+    solution = Solution()
+    result = solution.wiggleMaxLength(nums=[1, 7, 4, 9, 2, 5])
+    print(result)
