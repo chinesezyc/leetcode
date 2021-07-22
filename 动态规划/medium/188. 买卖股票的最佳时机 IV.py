f6b5508da9e8793fdfd03e590a6a529e38eddd74
zyc <chinesexzyc@gmail.com>
2021-07-22 22:36:43 +0800

    188. 买卖股票的最佳时机 IV

--- 动态规划/medium/188. 买卖股票的最佳时机 IV.py
+++ 动态规划/medium/188. 买卖股票的最佳时机 IV.py
@@ -3,16 +3,22 @@ from typing import List
 
 class Solution:
     def maxProfit(self, k: int, prices: List[int]) -> int:
-        dp = [[0, 0] for _ in range(prices.__len__())]
-        dp[0][1] = -prices[0]
-        for i in range(1, prices.__len__()):
-            dp[i][0] = max(dp[i - 1][0], dp[i - 1][1] + prices[i])
-            dp[i][1] = max(dp[i - 1][1], dp[i - 2][0] - prices[i])
+        if prices.__len__() < 2:
+            return 0
+        K = min(k, prices.__len__() // 2) + 1
+        dp = [[[0] * K, [0] * K] for _ in range(prices.__len__())]
+        for i in range(K):
+            dp[0][1][i] = -prices[0]
 
-        return dp[-1][0]
+        for i in range(1, prices.__len__()):
+            for j in range(1, K):
+                dp[i][0][j] = max(dp[i - 1][0][j], dp[i - 1][1][j] + prices[i])
+                dp[i][1][j] = max(dp[i - 1][1][j], dp[i - 1][0][j - 1] - prices[i])
+
+        return dp[-1][0][-1]
 
 
 if __name__ == "__main__":
     solution = Solution()
-    result = solution.maxProfit(k = 2, prices = [3,2,6,5,0,3])
+    result = solution.maxProfit(k=2, prices=[3, 2, 6, 5, 0, 3])
     print(result)
