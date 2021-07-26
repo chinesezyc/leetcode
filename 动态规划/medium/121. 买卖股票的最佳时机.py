1f1c3b7bcb6c4bba0fa38f0f7868833cc3af9c41
zyc <chinesexzyc@gmail.com>
2021-07-26 22:58:16 +0800

    121. 买卖股票的最佳时机 dp

--- 动态规划/medium/121. 买卖股票的最佳时机.py
+++ 动态规划/medium/121. 买卖股票的最佳时机.py
@@ -3,10 +3,14 @@ from typing import List
 
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-
+        dp_i_0, dp_i_1 = 0, -prices[0]
+        for i in range(1, prices.__len__()):
+            dp_i_0 = max(dp_i_0, dp_i_1 + prices[i])
+            dp_i_1 = max(dp_i_1, -prices[i])
+        return dp_i_0
 
 
 if __name__ == "__main__":
     solution = Solution()
-    result = solution.maxProfit([7,1,5,3,6,4])
+    result = solution.maxProfit([7, 1, 5, 3, 6, 4])
     print(result)
