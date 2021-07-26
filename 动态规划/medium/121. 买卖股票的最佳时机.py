ab1737ed97a7c43ac8e5c3526e5a162f3c543f04
zyc <chinesexzyc@gmail.com>
2021-07-26 22:54:16 +0800

    121. 买卖股票的最佳时机

---
+++ 动态规划/medium/121. 买卖股票的最佳时机.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+
+
+
+if __name__ == "__main__":
+    solution = Solution()
+    result = solution.maxProfit([7,1,5,3,6,4])
+    print(result)
