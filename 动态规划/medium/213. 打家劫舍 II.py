36e88363da9c74b7db74f683350e7279cb9b53f2
zyc <chinesexzyc@gmail.com>
2021-07-28 23:31:40 +0800

    213. 打家劫舍 II

--- 动态规划/medium/213. 打家劫舍 II.py
+++ 动态规划/medium/213. 打家劫舍 II.py
@@ -5,20 +5,19 @@ class Solution:
     def rob(self, nums: List[int]) -> int:
         if nums.__len__() <= 2:
             return max(nums)
+        def fun(sub_nums: List[int]) -> int:
+            if sub_nums.__len__() <= 2:
+                return max(sub_nums)
+            dp = [0] * sub_nums.__len__()
+            dp[0], dp[1] = sub_nums[0], max(sub_nums[:2])
+            for i in range(2, sub_nums.__len__()):
+                dp[i] = max(dp[i - 1], dp[i - 2] + sub_nums[i])
+            return dp[-1]
 
-        for k in range(nums.__len__()):
-            dp = [0] * nums.__len__()
-            k = k % nums.__len__()
-            dp[k + 0], dp[k + 1] = nums[k+0], max(nums[k:k+2])
-            for i in range(k + 2, k+nums.__len__()):
-                i = i % nums.__len__()
-
-                dp[i] = max(dp[i - 1], dp[i - 2] + nums[i])
-
-        return dp[-1]
+        return max(fun(nums[:-1]), fun(nums[1:]))
 
 
 if __name__ == "__main__":
     solution = Solution()
-    result = solution.rob([3, 2, 6, 5, 0, 3])
+    result = solution.rob([3,4,3])
     print(result)
