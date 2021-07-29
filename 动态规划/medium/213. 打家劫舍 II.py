78d748581b5bee0229a3b2c69f64cab33dd1e1e1
zyc <chinesexzyc@gmail.com>
2021-07-29 23:14:11 +0800

    213. 打家劫舍 II

--- 动态规划/medium/213. 打家劫舍 II.py
+++ 动态规划/medium/213. 打家劫舍 II.py
@@ -5,10 +5,13 @@ class Solution:
     def rob(self, nums: List[int]) -> int:
         if nums.__len__() <= 2:
             return max(nums)
-        dp = [0] * nums.__len__()
-        dp[0], dp[1] = nums[0], max(nums[:2])
-        for i in range(2, nums.__len__()):
-            dp[i] = max(dp[i - 1], dp[i - 2] + nums[i])
+
+        for k in range(nums.__len__()):
+            dp = [0] * nums.__len__()
+            k = k % nums.__len__()
+            dp[k + 0], dp[k + 1] = nums[0], max(nums[:2])
+            for i in range(k + 2, nums.__len__()):
+                dp[i] = max(dp[i - 1], dp[i - 2] + nums[i])
 
         return dp[-1]
 
