be5b2d7499741cd15ca5040fab9abeeeb3f0294c
zyc <chinesexzyc@gmail.com>
2021-08-01 23:06:57 +0800

    198. 打家劫舍

--- 动态规划/medium/198. 打家劫舍.py
+++ 动态规划/medium/198. 打家劫舍.py
@@ -1,3 +1,6 @@
+from typing import List
+
+
 class Solution:
     def rob(self, nums: List[int]) -> int:
         if nums.__len__() <= 2:
@@ -8,3 +11,9 @@ class Solution:
             dp[i] = max(dp[i - 1], dp[i - 2] + nums[i])
 
         return dp[-1]
+
+
+if __name__ == "__main__":
+    solution = Solution()
+    result = solution.rob([3, 2, 6, 5, 0, 3])
+    print(result)
