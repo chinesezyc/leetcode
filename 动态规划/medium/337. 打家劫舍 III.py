b16ca2b4eb5909011590b34388ef8b7b2d30f7f6
zyc <chinesexzyc@gmail.com>
2021-08-03 23:29:03 +0800

    337. 打家劫舍 III

--- 动态规划/medium/337. 打家劫舍 III.py
+++ 动态规划/medium/337. 打家劫舍 III.py
@@ -10,3 +10,15 @@ class TreeNode:
 
 class Solution:
     def rob(self, root: TreeNode) -> int:
+        f, g = {None: 0}, {None: 0}
+
+        def dfs(node: TreeNode):
+            if node is None:
+                return None
+            dfs(node.left)
+            dfs(node.right)
+            f[node] = g[node.left] + g[node.right] + node.val
+            g[node] = max(f[node.left], g[node.left]) + max(f[node.right], g[node.right])
+
+        dfs(root)
+        return max(f[root], g[root])
